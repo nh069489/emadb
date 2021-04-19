93c6d481a648d7487f3c5b091c0cdb7818c3483f
@nh069469 <nh069489@gmail.com>
2021-04-20 02:31:14 +0600

    delete readme

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+const Inventory = () => {
+    const handleAddProduct =() => {
+        const product = {}  
+        fetch('http://localhost:5000/addProduct', {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+    }
+    return (
+        <div>
+             <form action="">
+                 <p><span>Name:</span><input type="text"/></p>
+                 <p><span>price:</span><input type="text"/></p>
+                 <p><span>Quantity:</span><input type="text"/></p>
+                 <p><span>ProductImage:</span><input type="file"/></p>
+                <button onClick={handleAddProduct}>Add Product</button>
+             </form>
+        </div>
+    );
+};
+
+export default Inventory;
